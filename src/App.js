275e5c9ac30d0da29d336c339dc32e949ce9061b
merili132 <merilikovask0@gmail.com>
2024-02-27 19:45:49 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import"./Luuletus.css"
+import Luuletus from './Luuletus';
+
+function App() {
+  return (
+    <div className="konteiner">
+      <h1>Luuletus</h1>
+      <br />
+      <Luuletus />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Luuletus.js
@@ -0,0 +1,14 @@
+function Luuletus() {
+    return (
+        <>
+            <p>Kes hommikuti külas käib,</p>
+            <p>see asjatult ei longi</p>
+            <p>sest selleks hommik ongi</p>
+        </>
+        
+    )
+
+
+}
+
+export default Luuletus
